34073fc78d0816f2823c5f4da3c9faa0e38eec05
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-11-19 08:27:36 -0600

    added an ID GUID to the saved recipe object, RecipeWithImage

---
+++ AIWonderRecipe/Shared/RecipeWithImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIWonderRecipe.Shared
+{
+    public class RecipeWithImage
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); //Auto generate ID
+        public Recipe? Recipe { get; set; }
+        public string? ImageUrl { get; set; } // URL from ImageData
+        public string? MealTime { get; set; } // store MealTime category
+    }
+
+}
